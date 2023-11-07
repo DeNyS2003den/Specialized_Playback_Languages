c4e3d759b43d1457f728013d336d6870785e8141
DeNyS2003den <145554980+DeNyS2003den@users.noreply.github.com>
2023-11-07 16:15:21 +0200

    Update main.py

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,1 +1,28 @@
+from CustomArt import CustomArt
+
+font = {
+    'm': '1',
+    'e':'2',
+    's':'3'
+}
+art = CustomArt('hp')
+
+# art._size_x = 10
+# art._size_y = 12
+# art.zoom()
+# print(art.prev_view())
+#
+art.justify = 'right'
+art.create()
+print(art)
+
+art.justify = 'center'
+art.create()
+print(art)
+
+
+art.justify = 'left'
+art.create()
+print(art)
+
 
